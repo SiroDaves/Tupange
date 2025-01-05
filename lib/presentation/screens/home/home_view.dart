e1ee7b2a59be7ab86371aa371d9e3ef37bca812d
Siro Daves <sirodevske@gmail.com>
2025-01-05 23:33:45 +0300

    minor refactors

--- lib/presentation/screens/home/home_view.dart
+++ lib/presentation/screens/home/home_view.dart
@@ -9,7 +9,8 @@ class HomeView extends StatefulWidget {
 
 class HomeViewState extends State<HomeView> {
   Size get size => MediaQuery.of(context).size;
-
+  List<Category> categories = [];
+  List<Game> games = [];
   @override
   void initState() {
     super.initState();
@@ -25,61 +26,65 @@ class HomeViewState extends State<HomeView> {
   Widget build(BuildContext context) {
     return BlocConsumer<HomeBloc, HomeState>(
       listener: (context, state) {
-        if (state is HomeFailureState) {
+        if (state is HomeFetchedState) {
+          categories = state.categories;
+          games = state.games;
+        } else if (state is HomeFailureState) {
           CustomSnackbar.show(context, state.feedback);
         }
       },
       builder: (context, state) {
+        var mainStack = Stack(
+          children: [
+            ResponsiveLayoutBuilder(
+              small: (_, Widget? child) => HomeSmall(child: child!),
+              medium: (_, Widget? child) => HomeMedium(child: child!),
+              large: (_, Widget? child) => child!,
+              child: (_) => HomeDetails(
+                categories: categories,
+                games: games,
+              ),
+            ),
+            const HeaderWidget(),
+            ResponsiveLayoutBuilder(
+              small: (_, __) => const SizedBox.shrink(),
+              medium: (_, __) => const SizedBox.shrink(),
+              large: (_, __) => const Align(
+                alignment: AppConstants.kFOTopRight,
+                child: AudioControl(),
+              ),
+            ),
+            // const Align(
+            //   alignment: AppConstants.kFOBottomRight,
+            //   child: AnimationToggleButton(),
+            // ),
+            ResponsiveLayoutBuilder(
+              small: (_, __) => const Align(
+                alignment: AppConstants.kFOBottomLeft,
+                child: InfoButton(),
+              ),
+              medium: (_, __) => const Align(
+                alignment: AppConstants.kFOTopLeft,
+                child: InfoButton(),
+              ),
+              large: (_, __) => const Align(
+                alignment: AppConstants.kFOTopLeft,
+                child: InfoButton(),
+              ),
+            ),
+          ],
+        );
         return SizedBox.fromSize(
           size: size,
           child: state.maybeWhen(
-            orElse: () => const SizedBox(),
+            orElse: () => mainStack,
             failure: (feedback) => EmptyState(
               //title: l10n.habitChooserFailure,
               showRetry: true,
               //onRetry: () => bloc.add(const FetchData()),
             ),
             loading: () => LoadingProgress(),
-            fetched: (categories, games) => Stack(
-              children: [
-                ResponsiveLayoutBuilder(
-                  small: (_, Widget? child) => HomeSmall(child: child!),
-                  medium: (_, Widget? child) => HomeMedium(child: child!),
-                  large: (_, Widget? child) => child!,
-                  child: (_) => HomeDetails(
-                    categories: categories,
-                    games: games,
-                  ),
-                ),
-                const HeaderWidget(),
-                ResponsiveLayoutBuilder(
-                  small: (_, __) => const SizedBox.shrink(),
-                  medium: (_, __) => const SizedBox.shrink(),
-                  large: (_, __) => const Align(
-                    alignment: AppConstants.kFOTopRight,
-                    child: AudioControl(),
-                  ),
-                ),
-                // const Align(
-                //   alignment: AppConstants.kFOBottomRight,
-                //   child: AnimationToggleButton(),
-                // ),
-                ResponsiveLayoutBuilder(
-                  small: (_, __) => const Align(
-                    alignment: AppConstants.kFOBottomLeft,
-                    child: InfoButton(),
-                  ),
-                  medium: (_, __) => const Align(
-                    alignment: AppConstants.kFOTopLeft,
-                    child: InfoButton(),
-                  ),
-                  large: (_, __) => const Align(
-                    alignment: AppConstants.kFOTopLeft,
-                    child: InfoButton(),
-                  ),
-                ),
-              ],
-            ),
+            fetched: (categories, games) => mainStack,
           ),
         );
       },
