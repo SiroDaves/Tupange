2dc348426d5825ce7e42b97cb4ba60183aae4e9d
Siro Daves <sirodevske@gmail.com>
2025-03-30 22:38:51 +0300

    flutter_dotenv additon

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,14 @@ import 'dart:async';
 
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 import 'app.dart';
 import 'core/di/injectable.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  //await dotenv.load(fileName: ".env");
+  await dotenv.load(fileName: ".env");
   
   //final supabaseUrl = dotenv.env['SUPABASE_URL'];
   //final supabaseAnonKey = dotenv.env['SUPABASE_ANON_KEY'];
