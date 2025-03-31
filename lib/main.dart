e83d3ac0deb030ee06481428215de2aac513cdcd
Siro Daves <sirodevske@gmail.com>
2025-03-31 04:54:46 +0300

    supabase intergration

--- lib/main.dart
+++ lib/main.dart
@@ -3,19 +3,21 @@ import 'dart:async';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:supabase_flutter/supabase_flutter.dart' as supa;
 
 import 'app.dart';
 import 'core/di/injectable.dart';
+import 'core/utils/app_utils.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await dotenv.load(fileName: ".env");
   
-  //final supabaseUrl = dotenv.env['SUPABASE_URL'];
-  //final supabaseAnonKey = dotenv.env['SUPABASE_ANON_KEY'];
+  final supabaseUrl = dotenv.env['SUPABASE_URL'];
+  final supabaseAnonKey = dotenv.env['SUPABASE_ANON_KEY'];
 
-  //await supa.Supabase.initialize(url: supabaseUrl!, anonKey: supabaseAnonKey!);
-  //AppUtils.logger('Supabase init started: $supabaseUrl');
+  await supa.Supabase.initialize(url: supabaseUrl!, anonKey: supabaseAnonKey!);
+  AppUtils.logger('Supabase init started: $supabaseUrl');
   await configureDependencies(kIsWeb ? 'dev' : 'prod');
   runApp(const MyApp());
 }
